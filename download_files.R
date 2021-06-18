78fdee15baf673f156439508e7c1daa37a8aa086
Bartłomiej Tyrcha <bartlomiejtyrcha@gmail.com>
2021-06-18 15:08:26 +0200

    Kopia danych, notatki

---
+++ download_files.R
@@ -0,0 +1,43 @@
+
+#install.packages("xml2")
+#install.packages("rvest")
+
+library(xml2)
+library(rvest)
+library(stringr)
+### Dane do strony
+
+strona = xml2::read_html("https://www.ztm.poznan.pl/pl/dla-deweloperow/gtfsFiles")
+linki = rvest::html_table(strona)[[2]]
+
+### Pobieranie danych - pojedynczy plik
+
+download.file("https://www.ztm.poznan.pl/pl/dla-deweloperow/getGTFSFile/?file=20210125_20210221.zip", 
+              destfile=str_c(getwd(),"/TEST"))
+
+View(linki)
+linki[1]
+nrow(linki)
+linki = linki[-nrow(linki),]
+is.character(linki[267,1])
+str(linki)
+#str_detect(linki[,1], pattern = "\\.")
+View(linki[,1])
+
+linki2 = linki
+#linki2[1,1] = str_c("https://www.ztm.poznan.pl/pl/dla-deweloperow/getGTFSFile/?file=", linki2[1,1]) - DZIAŁA
+
+### AUTOMATYZACJA POBIERANIA 
+
+linki$file_www = str_c("https://www.ztm.poznan.pl/pl/dla-deweloperow/getGTFSFile/?file=", linki$`Nazwa pliku`)
+linki = linki[-nrow(linki),]
+dir.create("dane")
+getwd() 
+
+######### TO POBIERA WSZYSTKIE PLIKI UWAGA XD
+for(i in 1:length((linki$`Nazwa pliku`))){
+  download.file(linki[i,5], destfile=str_c("F:/PROJEKTY/R/ztm/dane/", linki$`Nazwa pliku`[i]))
+} #pętla pobierania
+
+c(4,5,6,7)[2]
+
---
+++ notatki.R
@@ -0,0 +1,58 @@
+library(dplyr)
+library(mapview)
+library(sf)
+#install.packages('mapview')
+library(mapview)
+
+stops20181214_20181216 = read.delim2("unzip_files/20181214_20181216/stops.txt", sep=",")
+View(stops20181214_20181216)
+#(20181214_20181216)
+temp = list.files(path = "unzip_files/20181214_20181216",pattern="*.txt", full.names = T)
+agency = read.delim2(temp[1], sep=",")
+calendar_dates = read.delim2(temp[2], sep=",")
+calendar = read.delim2(temp[3], sep=",")
+feed_info = read.delim2(temp[4], sep=",")
+routes = read.delim2(temp[5], sep=",")
+shapes = read.delim2(temp[6], sep=",")
+stop_times = read.delim2(temp[7], sep=",")
+stops = read.delim2(temp[8], sep=",")
+trips = read.delim2(temp[9], sep=",")
+
+
+#agency
+View(agency)
+#calendar dates
+View(calendar_dates) # nic nie ma XD
+# calendar - 0 - kurs sie nie odbywa, 1 - odbywa sie
+View(calendar)
+# feed_info - kto udostepnia dane
+View(feed_info)
+### routes - 
+#agency_id - kto obsługuje linie
+#route_short_name - numer linii/ krótka nazwa linii
+View(routes)
+### shapes - kształty linii
+View(shapes) # trochę ich jest XD shape_id to kształt przebiegu linii
+shapes_sf = st_as_sf(shapes, coords = c("shape_pt_lon", "shape_pt_lat"))
+shapes_sf = st_set_crs(shapes_sf, 4326)
+plot(shapes_sf["shape_id"])
+###stop times
+# no i tu grubo XD pierwsza kolumna:
+# ^ - Oznaczenie danego kursu znacznikiem legendy, powiązującym go z polem route_desc w pliku routes.txt – 
+# dzięki temu na tabliczce rozkładów jazdy oznaczyć można kursy,których dotyczy opis w legendzie pod trasą
+#przejazdu linii. Jeśli znaczników jest kilka,należy oddzielić jeprzecinkiem. Przypadek
+#oznaczenia ważnego tylko na części kursu:  przed znakiem określa się numer przystanku od
+#którego oznaczenie obowiązuje, a po znaku – do którego. Przykład:
+#  G:2:8 – oznaczenie G znajduje się na przystankach od 2 do 8 (włącznie) danego kursu.
+#Powiązane z trips.txt. + - oznaczenie wariantu głównego 
+View(stop_times)
+
+## sf
+# stops
+stops_sf = st_as_sf(stops, coords = c("stop_lon", "stop_lat"))
+stops_sf = st_set_crs(stops_sf, 4326)
+View(stops_sf)
+plot(stops_sf)
+mapview(stops_sf)
+View(stop_times)
+# 
